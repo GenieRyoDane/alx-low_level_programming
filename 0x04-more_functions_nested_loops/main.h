5013042a96e7499b9b3d58851315c99ead545f46
GenieRyoDane <ayoubpxll@gmail.com>
2023-06-22 10:12:49 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include "_putchar.c"
-#include <stdio.h>
 int _putchar(char c);
+int main(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,5 +10,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
-#endif
